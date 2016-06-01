fda205406559182a72400eca167799ba812f6a4b
EdwinPB <edwinpolo@efrens-imac.local>
2016-06-01 16:51:19 -0500

    Interactive Story model complete

--- InteractiveStory/InteractiveStory.swift
+++ InteractiveStory/InteractiveStory.swift
@@ -17,7 +17,7 @@ enum Story: String {
 
 extension Story {
     var artwork: UIImage {
-    return UIImage(named: self.rawValue)!
+        return UIImage(named: self.rawValue)!
     }
     
     var text: String {
@@ -42,7 +42,7 @@ extension Story {
             return "thing"
         }
     }
-
+    
 }
 
 
@@ -57,7 +57,50 @@ class Page {
     }
 }
 
+extension Page {
+    
+    func addChoice (title: String, story: Story) -> Page{
+        let page = Page(story: story)
+        return addChoice(title, page: page)
+        
+    }
+    
+    func addChoice(title: String, page: Page) -> Page {
+        switch (firstChoice, secondChoice){
+        case (.Some, .Some): break
+        case (.None, .None), (.None, .Some):
+            firstChoice = (title, page)
+        case (.Some, .None):
+            secondChoice = (title, page)
+            
+        }
+        return page
+    }
+}
 
+struct Adventure {
+    static var story: Page {
+        let returnTrip = Page(story: .ReturnTrip)
+        let touchdown = returnTrip.addChoice("Stop and investigate", story: .TouchDown)
+        let homeward = returnTrip.addChoice("Continue Home to Earth", story: .Homeward)
+        let rover = returnTrip.addChoice("Explore the rover", story: .Rover)
+        let crate = returnTrip.addChoice("Open the crate", story: .Crate)
+        
+        homeward.addChoice("Head back to mars", page: touchdown)
+        let home = rover.addChoice("Continue towards", story: .Home)
+        
+        let cave = rover.addChoice("Explore the Coordinates", story: .Cave)
+        rover.addChoice("Return to Earth", page: home)
+        
+        cave.addChoice("Continue towards faint", story: .Droid)
+        cave.addChoice("Refill the ship", page: rover)
+        
+        crate.addChoice("Explore the Rover", page: rover)
+        crate.addChoice("Use the key", story: .Monster)
+        
+        return returnTrip
+    }
+}
 
 
 
